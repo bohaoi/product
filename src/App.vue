5e2e67d074a346cdc17a13696c71770fb0bbf847
bohaoi <bohao8485@163.com>
2020-07-28 11:18:28 +0800

    navigation

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,30 @@
 html,body {
   width: 100%;
   height: 100%;
-  /* margin: 0px;
-  padding: 0px; */
 }
 #app{
   width: 100%;
   height: 100%;
+}
+
+::-webkit-scrollbar-track
+{
+  background: rgba(0,0,0,.1);
+  border-radius: 0;
+}
+
+::-webkit-scrollbar
+{
+  -webkit-appearance: none;
+  width: 10px;
+  height: 10px;
+}
+
+::-webkit-scrollbar-thumb
+{
+    cursor: pointer;
+    border-radius: 5px;
+    background: rgba(0,0,0,.25);
+    transition: color .2s ease;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//引入全局配置文件
+import $conf from "./common/config/config"
+Vue.prototype.$conf = $conf
+
 //引入element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
