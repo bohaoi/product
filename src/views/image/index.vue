5d95883d51c68ff4ef892650b6c4646796ece6ef
bohaoi <bohao8485@163.com>
2020-08-07 09:29:02 +0800

    second submit album

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -23,7 +23,7 @@
           ></el-input>
           <el-button type="success" size="mini">搜索</el-button>
         </div>
-        <el-button type="success" size="mini">创建相册</el-button>
+        <el-button type="success" @click="openAlbumModel(false)" size="mini">创建相册</el-button>
         <el-button type="warning" size="mini">上传图片</el-button>
       </el-header>
 
@@ -31,29 +31,27 @@
         <el-aside width="200px" style="position:absolute;top:60px;left:0;bottom:60px;">
           <!--侧边 | 相册列表-->
           <ul class="list-group list-group-flush">
-            <li class="list-group-item list-group-item-action d-flex aligin-items-center" style="cursor:pointer"
-            v-for="(item,index) in albums" :key="index"
-            :class="{'active sum-active':albumIndex === index}"
-            @click.stop="albumChange(index)">
+            <li
+              class="list-group-item list-group-item-action d-flex aligin-items-center"
+              style="cursor:pointer"
+              v-for="(item,index) in albums"
+              :key="index"
+              :class="{'active sum-active':albumIndex === index}"
+              @click.stop="albumChange(index)"
+            >
               {{item.name}}
               <el-dropdown class="ml-auto">
-                <span class="btn btn-light btn-sm border" style="width:56px;">{{item.num}} 
-                <i class="el-icon-arrow-down el-icon--right"></i>
-               </span>
+                <span class="btn btn-light btn-sm border" style="width:56px;">
+                  {{item.num}}
+                  <i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
                 <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>修改</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
+                  <el-dropdown-item @click.stop.native="openAlbumModel({item,index})">修改</el-dropdown-item>
+                  <el-dropdown-item @click.stop.native="albumDel(index)">删除</el-dropdown-item>
                 </el-dropdown-menu>
-
-
               </el-dropdown>
-
-
             </li>
-
-
           </ul>
-
         </el-aside>
         <el-container>
           <el-main style="position:absolute;top:60px;left:200px;bottom:60px;right:0;">
@@ -63,6 +61,22 @@
       </el-container>
       <el-footer>Footer</el-footer>
     </el-container>
+
+    <!--修改|创建相册-->
+    <el-dialog title="修改相册" :visible.sync="albumModel">
+      <el-form ref="form" :model="albumForm" label-width="80px">
+        <el-form-item label="相册名称">
+          <el-input v-model="albumForm.name" size="medium" placeholder="请输入相册名称"></el-input>
+        </el-form-item>
+        <el-form-item label="相册排序">
+          <el-input-number v-model="albumForm.order" :min="0" size="medium"></el-input-number>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="albumModel = false">取 消</el-button>
+        <el-button type="primary" @click="confirmAlbumModel">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -75,6 +89,12 @@ export default {
         keyword: "",
       },
       albumIndex: 0,
+      albumModel: false,
+      albumEditIndex: -1,
+      albumForm: {
+        name: "",
+        order: 0,
+      },
       albums: [],
     };
   },
@@ -92,17 +112,74 @@ export default {
       }
     },
     //切换相册
-    albumChange(index){
+    albumChange(index) {
       this.albumIndex = index;
-    }
+    },
+    //打开相册修改/创建框
+    openAlbumModel(obj) {
+      //修改
+      if (obj) {
+        let { item, index } = obj;
+        //初始化表单
+        this.albumForm.name = item.name;
+        this.albumForm.order = item.order;
+        this.albumEditIndex = index;
+        //打开模态框
+        return (this.albumModel = true);
+      }
+      //创建
+      this.albumForm = {
+        name: "",
+        order: 0,
+      };
+      this.albumEditIndex = -1;
+      this.albumModel = true;
+    },
+
+    //点击确定修改/创建相册
+    confirmAlbumModel() {
+      //判断是否为修改
+      if (this.albumEditIndex > -1) {
+        this.albumEdit();
+        return (this.albumModel = false);
+      }
+
+      //追加albums
+      this.albums.unshift({
+        name: this.albumForm.name,
+        orser: this.albumForm.order,
+        num: 0,
+      });
+      this.albumModel = false;
+    },
+
+    //修改相册
+    albumEdit() {
+      this.albums[this.albumEditIndex].name = this.albumForm.name;
+      this.albums[this.albumEditIndex].order = this.albumForm.order;
+    },
+    //删除相册
+    albumDel(index) {
+      this.$confirm("是否删除该相册", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(() => {
+        this.albums.splice(index, 1);
+        this.$message({
+          type: "success",
+          message: "删除成功!",
+        });
+      });
+    },
   },
 };
 </script>
 
 <style>
-.sum-active{
-  color: #67C23A!important;
-  background-color: #f0f9eb!important;
-  border-color: #c2e7b0!important;
+.sum-active {
+  color: #67c23a !important;
+  background-color: #f0f9eb !important;
+  border-color: #c2e7b0 !important;
 }
 </style>
